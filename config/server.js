4457a771786134947b32534fe3ebf3233dc9f247
Veeresh603 <veereshkavalagi31@gmail.com>
2021-03-15 14:28:18 +0530

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ba9557ea5a4938890f6da9c833a041ef'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0ff88acf-e36f-4fdc-99a4-dac57cf9027a'
+};
